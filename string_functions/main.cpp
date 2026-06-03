16fe446ca0cf8b76bc2001117697fc954fa92b18
EUGENIO\eugen <eugenionyepes@gmail.com>
2026-06-02 22:54:33 -0300

    clase 2/6

--- string_functions/main.cpp
+++ string_functions/main.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 
 using namespace std;
 
 int stringToInt(string s);
 int potencia(int base, int exponente);
 double stringToDouble(string s);
+std::vector<string> stringSplit(string s, char split);
 
 int main () {
-	int numero = stringToInt("50025");
+	// int numero = stringToInt("50025");
 
-	cout << numero;
+	// cout << numero;
+
+	vector<string> result = stringSplit("123.45", '.');
+
+	for (const auto& str : result) {
+		cout << str << endl;
+	}
 }
 
 int stringToInt(string s) {
@@ -18,7 +26,7 @@ int stringToInt(string s) {
 	int numero = 0;
 	for (int i = 0; i < s.size(); i++) {
 		char letra = s[i];
-		numero += (letra - '0') * potencia(10, contPotencia);
+		numero += (letra - '0') * potencia(10, contPotencia); // numero = numero + algo
 		contPotencia--;
 	}
 
@@ -28,6 +36,22 @@ int stringToInt(string s) {
 double stringToDouble(string s) {
 	// buscar el . de la string, luego del punto, utilizar potencias negativas. por ejemplo 10 ^ -1 =  0.1.
 	// utilizar la funcion pow, porque la funcion q desarrollamos es solo para potencias positivas.
+	vector<string> partes = stringSplit(s, '.');
+}
+
+std::vector<string> stringSplit(string s, char split) {
+	vector<string> result;
+	string aux = "";
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == split) {
+			result.push_back(aux);
+			aux = "";
+		} else {
+			aux += s[i];
+		}
+	}
+	result.push_back(aux);
+	return result;
 }
 
 int potencia(int base, int exponente) {
